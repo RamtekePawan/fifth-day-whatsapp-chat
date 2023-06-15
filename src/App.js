4f2c1077ca5552b8ef81c3a3c0dd2fea61a9c940
PAWAN RAMTEKE <pawanramteke98@gmail.com>
2023-06-15 15:26:17 +0530

    Message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-primary text-center row justify-content-center">
+        <h1 className="bg-secondary  col-6  ">Hello WhatsAPP</h1>
+        <h1 className="text-light">PROPOS</h1>
+      </div>
+      <div className="m-4">
+        <MessageApp />
+        
+        <MessageApp name="Pawan" message="How are You?" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis, doloribus obcaecati non nobis autem amet asperiores iste. Velit, consequatur." />
+
+        <MessageApp name='Pankaj' message="fine Bro!!" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis, doloribus obcaecati non nobis autem amet asperiores iste. Velit, consequatur." />
+
+        <MessageApp name="Pradip " message="Chill Bro" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis, doloribus obcaecati non nobis autem amet asperiores iste. Velit, consequatur." />
+
+        <MessageApp name="Nitesh " message="Nothing" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis, doloribus obcaecati non nobis autem amet asperiores iste. Velit, consequatur." />
+      </div>
+    </>
+
+  )
+}
+
+function MessageApp1(props) {
+  return (
+    <>
+      <div className="alert alert-primary"><h2>
+        Hii {props.name}
+      </h2>
+        <h3>{props.message}</h3>
+        <p>{props.desc}</p>
+      </div>
+    </>
   );
 }
+function MessageApp({desc,name,message}) {
+  return (
+    <>
+      <div className="alert alert-danger"><h2>
+        Hi {name}
+      </h2>
+        <h3>{message}</h3>
+        <p>{desc}</p>
+      </div>
+    </>
+  );
+}
+
+
 
 export default App;
